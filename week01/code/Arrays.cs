34944d6e325717decb5b08886f38b664579ebe8a
Andres Pluska <pluskaandres@gmail.com>
2024-11-09 13:37:58 -0500

    feat(week01): code - dynamic array

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,24 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // I'm going to use an array to store the information.
+        // then a loop that iterates the number of times that is “length”.
+        // I will use the index of the loop to multiply with “number” and store the result in the array.
+
+
+        double[] numbers = new double[length];
+
+        for (int i = 1; i <= length; i++)
+        {
+            double temp;
+
+            temp = number * i;
+
+            numbers.SetValue(temp, i-1);
+
+        }
+
+        return numbers; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +46,25 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // First, using % I will determinate if the amount is in the right range
+        // I will use a conditions to verify if the rotation is needed
+        // I will use two new lists for this exercise
+        // The first will be elementsMoved, which will use the function GetRange with the first element that know should go at the end
+        // the second will be elementsRotated, which will use the functions GetRange to get the elements should be now at the beggining
+        // Then I will clear "data" list
+        // And use AddRange to insert elementsMoved and elementsRotated into "data"
+
+        int rotateBy = amount % data.Count;
+        if (rotateBy == 0)
+        {
+            return;
+        }
+        List<int> elementsMoved = data.GetRange(data.Count - rotateBy, rotateBy);
+        List<int> elementsRotated = data.GetRange(0, data.Count - rotateBy);
+        data.Clear();
+        data.AddRange(elementsMoved);
+        data.AddRange(elementsRotated);
+
     }
 }
